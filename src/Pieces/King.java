cfaea5777f15abd7abc1cf9eef654b9f9b424b86
XxNE0XX <kiancoder1@gmail.com>
2018-05-24 22:11:52 +0430

    mainMenu has been Renamed

--- src/Pieces/King.java
+++ src/Pieces/King.java
@@ -15,10 +15,10 @@ public class King extends Piece {
     super.selected();
         for (int i = -1; i < 2; i++){
             for (int j = -1; j < 2; j++){
-                if (i == 0 && j == 0){
+                if (i == 0 && j == 0) {
                     continue;
                 }
-                else {
+                else if (Main.board.getCells(this.cell.getRow() + i, this.cell.getColumn() + j) != null) {
                     if (Main.board.getCells(this.cell.getRow() + i, this.cell.getColumn() + j).isEmpty())
                         Main.board.getCells(this.cell.getRow() + i, this.cell.getColumn() + j).setPossible(true);
                     else if (!Main.board.getCells(this.cell.getRow() + i, this.cell.getColumn() + j).isEmpty() &&
