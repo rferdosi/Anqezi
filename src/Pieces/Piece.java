456f40c1e7e339dc788f7fcea1fa6188e84a96cc
XxNE0XX <kiancoder1@gmail.com>
2018-05-24 21:23:25 +0430

    Piece CleanTexture Method added

--- src/Pieces/Piece.java
+++ src/Pieces/Piece.java
@@ -1,6 +1,7 @@
 package Pieces;
 
 import Board.Side;
+import Controllers.Main;
 import javafx.scene.image.ImageView;
 import Board.Cell;
 
@@ -15,10 +16,17 @@ abstract public class Piece {
     }
 
     public void selected(){
+        cleanTextures();
         this.cell.setSelected(true);
     }
 
     public void cleanTextures(){
-
+        for (int i = 0; i < 8; i++){
+            for (int j = 0; j < 8; j++){
+                Main.board.getCells(this.cell.getRow() + i, this.cell.getColumn() + j).setPossible(false);
+                Main.board.getCells(this.cell.getRow() + i, this.cell.getColumn() + j).setThreaten(false);
+                Main.board.getCells(this.cell.getRow() + i, this.cell.getColumn() + j).setSelected(false);
+            }
+        }
     }
 }
