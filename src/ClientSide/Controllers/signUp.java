d37574df2fb4591f53476dcbda245bf5f9d67c16
rferdosi <rferdosy@gmail.com>
2018-06-18 15:56:15 +0430

    sign up page added

--- src/ClientSide/Controllers/signUp.java
+++ src/ClientSide/Controllers/signUp.java
@@ -1,13 +1,21 @@
 package ClientSide.Controllers;
 
 import ClientSide.Client;
+import General.User.User;
 import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class signUp {
+    public TextField name;
+    public TextField username;
+    public PasswordField password;
+    public TextField age;
+    public TextField email;
     private static final Pattern VALID_EMAIL_ADDRESS_REGEX =
             Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 
@@ -35,6 +43,18 @@ public class signUp {
     }
 
     public void signUp(ActionEvent actionEvent) {
-        if ()
+        String nameStr = name.getText();
+        String usernameStr = username.getText();
+        String emailStr = email.getText();
+        String passwordStr = password.getText();
+        int ageInt = Integer.parseInt(age.getText());
+        try {
+            if (nameValidate(nameStr) && usernameValidate(usernameStr)
+                    && passwordValidate(passwordStr) && emailValidate(emailStr)) {
+                User user = new User(nameStr, usernameStr, emailStr, passwordStr, ageInt);
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
