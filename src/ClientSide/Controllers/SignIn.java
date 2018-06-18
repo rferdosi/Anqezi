a65b218119f9237b15ddf472e074f19dcc11e71a
XxNE0XX <kiancoder1@gmail.com>
2018-06-18 15:05:11 +0430

    SignIn FXML fixed

--- src/ClientSide/Controllers/SignIn.java
+++ src/ClientSide/Controllers/SignIn.java
@@ -1,14 +1,20 @@
 package ClientSide.Controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
 
-import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class SignIn extends FatherController {
+    @FXML
     public TextField userName;
-    public TextField password;
+    @FXML
+    public PasswordField password;
+    @FXML
     public Label message;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
