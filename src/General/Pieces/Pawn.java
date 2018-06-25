30a3c5ff332dd847f2905b39ba06409580b1733c
XxNE0XX <kiancoder1@gmail.com>
2018-06-25 19:57:27 +0430

    Pawn has fixed oskol 8)

--- src/General/Pieces/Pawn.java
+++ src/General/Pieces/Pawn.java
@@ -44,7 +44,6 @@ public class Pawn extends Piece {
                 current = cell.getDownCell();
                 if (current.isEmpty()) {
                     current.setPossible(true);
-                    current.getDownCell().setPossible(true);
                     if (isFirstMove) {
                         if (current.getDownCell().isEmpty()) {
                             current.getDownCell().setPossible(true);
