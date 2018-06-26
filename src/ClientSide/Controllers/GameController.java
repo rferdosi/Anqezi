7224acf23163bef96d032aaa1fb7b12a06b3c67a
XxNE0XX <kiancoder1@gmail.com>
2018-06-26 22:04:50 +0430

    time FXML added

--- src/ClientSide/Controllers/GameController.java
+++ src/ClientSide/Controllers/GameController.java
@@ -4,11 +4,18 @@ import General.Board.Board;
 import General.Board.Side;
 import General.Game;
 import General.User.Player;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ResourceBundle;
 
 public class GameController extends MotherController implements Initializable {
@@ -29,7 +36,8 @@ public class GameController extends MotherController implements Initializable {
         if (playerSide == Side.White)
             Board.isTurn = true;
         readFromBoard();
-
+        //Todo set maxTime here!
+        timeManagement(500000);
     }
 
     private void readFromBoard() {
@@ -44,6 +52,29 @@ public class GameController extends MotherController implements Initializable {
             }
         }
     }
+
+    private void timeManagement(long maxTime) {
+        long endTime = maxTime;
+        Label timeLabel = new Label();
+        DateFormat timeFormat = new SimpleDateFormat( "HH:mm:ss" );
+        final Timeline timeline = new Timeline(
+                new KeyFrame(
+                        Duration.millis( 500 ),
+                        event -> {
+                            final long diff = endTime - System.currentTimeMillis();
+                            if ( diff < 0 ) {
+                                //  timeLabel.setText( "00:00:00" );
+                                timeLabel.setText( timeFormat.format( 0 ) );
+                            } else {
+                                timeLabel.setText( timeFormat.format( diff ) );
+                            }
+                        }
+                )
+        );
+        timeline.setCycleCount( Animation.INDEFINITE );
+        timeline.play();
+    }
+
 }
 
 
